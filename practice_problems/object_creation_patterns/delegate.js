9722268bff027815080cfd4f60cb6311500c7ffc
Stephen Johnston <sdjohnston1990@gmail.com>
2022-01-12 19:41:46 -0500

    add Delegate exercise

---
+++ practice_problems/object_creation_patterns/delegate.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Delegate
+
+/*
+Write a delegate function that can be used to delegate the behavior of a 
+method or function to another object's method. delegate takes a minimum of 
+two arguments: (1) the object and (2) name of the method on the object. 
+The remaining arguments, if any, are passed — as arguments — to the objects' 
+method that it delegates to.
+
+Note that this is not the same as using bind. bind returns a new function, 
+whereas delegate maintains the reference.
+
+Here's a sample run;
+*/
+
+// implemented function
+function delegate(context, methodName, ...args) {
+  return function() {
+    return context[methodName](args);
+  }
+}
+
+const foo = {
+  name: 'test',
+  bar(greeting) {
+    console.log(`${greeting} ${this.name}`);
+  },
+};
+
+const baz = {
+  qux: delegate(foo, 'bar', 'hello'),
+};
+
+baz.qux();   // logs 'hello test';
+
+foo.bar = () => { console.log('changed'); };
+
+baz.qux();          // logs 'changed'
